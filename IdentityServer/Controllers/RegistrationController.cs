5fc00b0bf54830ce582d9b5c3822dab3ade84f0d
dmitrykotelev <dzmitry.kotseleu@innowise.com>
2026-05-03 17:41:01 +0300

    Add Logger

--- IdentityServer/Controllers/RegistrationController.cs
+++ IdentityServer/Controllers/RegistrationController.cs
@@ -15,7 +15,7 @@ namespace IdentityServer.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly IConfiguration _configuration;
-
+        private readonly ILogger<RegistrationController> _logger;
         public class RegisterModel
         {
             [Required]
@@ -25,22 +25,25 @@ namespace IdentityServer.Controllers
             public string Password { get; set; }
         }
 
-        public RegistrationController(UserManager<AppUser> userManager, IConfiguration configuration)
+        public RegistrationController(UserManager<AppUser> userManager, IConfiguration configuration, ILogger<RegistrationController> logger)
         {
             if (userManager != null)
                 _userManager = userManager;
             if (configuration != null)
                 _configuration = configuration;
+            _logger = logger;
         }
 
         [HttpPost("reg")]
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
+            _logger.LogInformation($"Registration start, Email - {model.Email}");
             var user = new AppUser { UserName = model.Email, Email = model.Email };
             var result = await _userManager.CreateAsync(user, model.Password);
 
             if (result.Succeeded)
             {
+                _logger.LogInformation($"Registration Succeed, Email -{model.Email}");
                 var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
                 var values = new { userId = user.Id, code = token };
@@ -51,24 +54,31 @@ namespace IdentityServer.Controllers
                 return Ok(new { Message = "User Registrated" });
             }
 
+            _logger.LogWarning($"Registration eror - {result.Errors}");
             return BadRequest(result.Errors);
         }
 
         [HttpGet("ConfirmEmail")]
         public async Task<IActionResult> ConfirmEmail(string userId, string code)
         {
+            _logger.LogInformation($"Confirm email started: {userId}");
+            
             var user = await _userManager.FindByIdAsync(userId);
             if (user == null)
             {
                 return NotFound();
             }
+
+            _logger.LogInformation($"User Founded, sending mail");
             var result = await _userManager.ConfirmEmailAsync(user, code);
 
             if (result.Succeeded)
             {
+                _logger.LogInformation($"Mail Sended");
                 return Ok(new { Message = "Email confirmed successfully" });
             }
 
+            _logger.LogWarning($"Failed to send email {result.Errors}");
             return BadRequest("Failed to confirm email.");
         }
 
