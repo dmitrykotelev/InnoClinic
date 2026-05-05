f418f0c0bca97df79ab3e33a6fba92f0fe651359
dmitrykotelev <dzmitry.kotseleu@innowise.com>
2026-05-05 09:04:55 +0300

    Fixed Logs

--- IdentityServer/Controllers/RegistrationController.cs
+++ IdentityServer/Controllers/RegistrationController.cs
@@ -1,9 +1,7 @@
 ﻿using IdentityServer.Helpers;
 using IdentityServerDatabase.Models;
-using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using MimeKit;
 using System.ComponentModel.DataAnnotations;
 using IdentityServer.Settings;
 
@@ -27,59 +25,63 @@ namespace IdentityServer.Controllers
 
         public RegistrationController(UserManager<AppUser> userManager, IConfiguration configuration, ILogger<RegistrationController> logger)
         {
-            if (userManager != null)
-                _userManager = userManager;
-            if (configuration != null)
-                _configuration = configuration;
-            _logger = logger;
+            _userManager = userManager ?? throw new ArgumentException(nameof(userManager));
+            _configuration = configuration ?? throw new ArgumentException(nameof(configuration));
+            _logger = logger ?? throw new ArgumentException(nameof(logger));
         }
 
         [HttpPost("reg")]
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
-            _logger.LogInformation($"Registration start, Email - {model.Email}");
-            var user = new AppUser { UserName = model.Email, Email = model.Email };
-            var result = await _userManager.CreateAsync(user, model.Password);
-
-            if (result.Succeeded)
+            using (_logger.BeginScope("UserEmail: {UserEmail}", model.Email))
             {
-                _logger.LogInformation($"Registration Succeed, Email -{model.Email}");
-                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                _logger.LogInformation($"Registration start, Email - ");
+                var user = new AppUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
 
-                var values = new { userId = user.Id, code = token };
-                var confirmationLink = Url.Action(nameof(ConfirmEmail), "Registration", values, protocol: Request.Scheme);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation($"Registration Succeed, Email - {user.Email}");
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
-                await SendGmailAsync(model.Email, confirmationLink);
+                    var values = new { userId = user.Id, code = token };
+                    var confirmationLink = Url.Action(nameof(ConfirmEmail), "Registration", values, protocol: Request.Scheme);
 
-                return Ok(new { Message = "User Registrated" });
+                    await SendGmailAsync(model.Email, confirmationLink);
+
+                    return Ok(new { Message = "User Registrated" });
+                }
+
+                _logger.LogWarning($"Registration eror - {result.Errors}");
+                return BadRequest(result.Errors);
             }
-
-            _logger.LogWarning($"Registration eror - {result.Errors}");
-            return BadRequest(result.Errors);
         }
 
         [HttpGet("ConfirmEmail")]
         public async Task<IActionResult> ConfirmEmail(string userId, string code)
         {
-            _logger.LogInformation($"Confirm email started: {userId}");
-            
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
+            using (_logger.BeginScope("UserEmail: {UserEmail}", userId))
             {
-                return NotFound();
+                _logger.LogInformation($"Confirm email started: {userId}");
+
+                var user = await _userManager.FindByIdAsync(userId);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                _logger.LogInformation($"User Founded, sending mail on adress{user.Email}");
+                var result = await _userManager.ConfirmEmailAsync(user, code);
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation($"Mail Sended on adress {user.Email}");
+                    return Ok(new { Message = "Email confirmed successfully" });
+                }
+
+                _logger.LogWarning($"Failed to send email on adress {user.Email} {result.Errors}");
+                return BadRequest("Failed to confirm email.");
             }
-
-            _logger.LogInformation($"User Founded, sending mail");
-            var result = await _userManager.ConfirmEmailAsync(user, code);
-
-            if (result.Succeeded)
-            {
-                _logger.LogInformation($"Mail Sended");
-                return Ok(new { Message = "Email confirmed successfully" });
-            }
-
-            _logger.LogWarning($"Failed to send email {result.Errors}");
-            return BadRequest("Failed to confirm email.");
         }
 
         private async Task SendGmailAsync(string targetEmail, string message)
